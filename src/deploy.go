48a72c1d049c95ca0ffe4557b767fd810bfb316b
Luminarys <kizunanohikari@gmail.com>
2015-09-06 00:04:31 -0500

    Added in git exceptions for deployment

--- src/deploy.go
+++ src/deploy.go
@@ -8,9 +8,9 @@ func deploy(path string, force bool) error {
 	cmd := "rsync"
 	var args []string
 	if force {
-		args = []string{"-r", path, homeDir + "/"}
+		args = []string{"-r", "--exclude", ".git/", path, homeDir + "/"}
 	} else {
-		args = []string{"-r", "--ignore-existing", path, homeDir + "/"}
+		args = []string{"-r", "--exclude", ".git/", "--ignore-existing", path, homeDir + "/"}
 	}
 	if err := exec.Command(cmd, args...).Run(); err != nil {
 		return err
